250fdb81af11751ff9c4bd8460f43f14d04dac40
Morten Laukvik <morten@laukvik.no>
2013-07-02 23:45:43 +0200

    Upgraded to java 1.6

--- src/main/java/org/laukvik/csv/CSVTableModel.java
+++ src/main/java/org/laukvik/csv/CSVTableModel.java
@@ -1,66 +1,81 @@
+/*
+ * Copyright 2013 Laukviks Bedrifter.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.laukvik.csv;
 
-import java.util.Vector;
-
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.TableModel;
 
 public class CSVTableModel implements TableModel {
-	
-	private CSV csv;
-	private Vector <TableModelListener> listeners;
-	
-	public CSVTableModel( CSV csv ){
-		this.csv = csv;
-		this.listeners = new Vector<TableModelListener>();
-	}
 
-	public void addTableModelListener( TableModelListener l ) {
-		this.listeners.add( l );
-	}
-	
-	public void removeTableModelListener(TableModelListener l) {
-		this.listeners.remove( l );
-	}
-	
-	public CSV getCSV() {
-		return csv;
-	}
+    private CSV csv;
+    private List<TableModelListener> listeners;
 
-	public Class<?> getColumnClass( int column ){
-		return String.class;
-	}
+    public CSVTableModel(CSV csv) {
+        this.csv = csv;
+        this.listeners = new ArrayList<TableModelListener>();
+    }
 
-	public int getColumnCount() {
-		return csv.getColumnCount();
-	}
+    public void addTableModelListener(TableModelListener l) {
+        this.listeners.add(l);
+    }
 
-	public String getColumnName(int column) {
-		if (csv.isHeadersAvailable()){
-			return csv.getHeader( column );
-		} else {
-			return null;
-		}
-	}
+    public void removeTableModelListener(TableModelListener l) {
+        this.listeners.remove(l);
+    }
 
-	public int getRowCount() {
-		return csv.getRowCount();
-	}
+    public CSV getCSV() {
+        return csv;
+    }
 
-	public Object getValueAt( int row, int column ) {
-		try {
-			return csv.getCell( column, row );
-		} catch (Exception e) {
-			return null;
-		}
-	}
+    public Class<?> getColumnClass(int column) {
+        return String.class;
+    }
 
-	public boolean isCellEditable(int row, int column ) {
-		return true;
-	}
+    public int getColumnCount() {
+        return csv.getColumnCount();
+    }
 
-	public void setValueAt( Object value, int row, int column ) {
-		csv.setCell( (String) value, column, row );
-	}
+    public String getColumnName(int column) {
+        if (csv.isHeadersAvailable()) {
+            return csv.getHeader(column);
+        } else {
+            return null;
+        }
+    }
 
+    public int getRowCount() {
+        return csv.getRowCount();
+    }
+
+    public Object getValueAt(int row, int column) {
+        try {
+            return csv.getCell(column, row);
+        }
+        catch (Exception e) {
+            return null;
+        }
+    }
+
+    public boolean isCellEditable(int row, int column) {
+        return true;
+    }
+
+    public void setValueAt(Object value, int row, int column) {
+        csv.setCell((String) value, column, row);
+    }
 }
