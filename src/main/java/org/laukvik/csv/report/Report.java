bd123a02b7ab386139ae43f95690f5195f8b5494
Morten Laukvik <ml@agitec.no>
2016-12-11 12:51:08 +0100

    First version that manages to build csv from node

--- src/main/java/org/laukvik/csv/report/Report.java
+++ src/main/java/org/laukvik/csv/report/Report.java
@@ -5,9 +5,12 @@ import org.laukvik.csv.Row;
 import org.laukvik.csv.columns.Column;
 import org.laukvik.csv.columns.IntegerColumn;
 import org.laukvik.csv.columns.StringColumn;
+import org.laukvik.csv.query.RowMatcher;
 import org.laukvik.csv.query.SortOrder;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 import java.util.Set;
 
@@ -24,6 +27,11 @@ public final class Report {
      * Group by.
      */
     private final List<Column> groups;
+
+    /**
+     * The list of row matchers.
+     */
+    private final List<RowMatcher> rowMatcherList;
     /**
      * Sort by.
      */
@@ -36,6 +44,7 @@ public final class Report {
         aggregateList = new ArrayList<>();
         groups = new ArrayList<>();
         sortOrderList = new ArrayList<>();
+        rowMatcherList = new ArrayList<>();
     }
 
     /**
@@ -66,6 +75,15 @@ public final class Report {
     }
 
     /**
+     * Adds a matcer.
+     *
+     * @param rowMatcher the rowmatcher
+     */
+    public void addMatcher(final RowMatcher rowMatcher) {
+        rowMatcherList.add(rowMatcher);
+    }
+
+    /**
      * Builds a new CSV with the aggregated data.
      *
      * @param csv the csv with the data
@@ -80,7 +98,7 @@ public final class Report {
         StringColumn sc2 = newCSV.addStringColumn(sc.getName());
 
         List<IntegerColumn> cols = new ArrayList<>();
-        for (Aggregate a : aggregateList){
+        for (Aggregate a : aggregateList) {
             IntegerColumn ic = new IntegerColumn(a.getColumn().getName());
             newCSV.addColumn(ic);
             cols.add(ic);
@@ -94,21 +112,17 @@ public final class Report {
             for (int y = 0; y < csv.getRowCount(); y++) {
                 Row row = csv.getRow(y);
                 String s = row.getString(sc);
-                if (s != null && s.equals(value)){
-//                    System.out.println("Unik: " + value  + " Found: " + s );
+                if (s != null && s.equals(value)) {
                     int z = 0;
                     for (Aggregate a : aggregateList) {
                         a.aggregate(row);
-                        if (a.getValue() instanceof Integer){
+                        if (a.getValue() instanceof Integer) {
                             Integer i = (Integer) a.getValue();
-                            System.out.println("Setting: " + i + " for " + value);
+//                            System.out.println("Setting: " + i + " for " + value);
                             newRow.setInteger(cols.get(z), i);
                         }
-
-//                        System.out.print(a.getValue() + "\t");
                         z++;
                     }
-//                    System.out.println();
                 }
             }
             // Reset aggregate after each unique
@@ -121,6 +135,131 @@ public final class Report {
         return newCSV;
     }
 
+    /**
+     * Builds a Node from the CSV.
+     *
+     * @param csv the CSV
+     * @return the Node
+     */
+    public Node buildNode(final CSV csv) {
+        Node root = new Node();
+        for (int y = 0; y < csv.getRowCount(); y++) {
+            Row r = csv.getRow(y);
+            Node extra = root;
+            // Exlude non matching rows
+            boolean matchesAll = true;
+            for (RowMatcher rm : rowMatcherList) {
+                if (!rm.matches(r)) {
+                    matchesAll = false;
+                }
+            }
+            //
+            if (matchesAll) {
+                for (int x = 0; x < groups.size(); x++) {
+                    Column c = groups.get(x);
+                    Object o = r.get(c);
+                    extra = extra.add(o, c);
+//                    IntegerColumn ic = null;
+//                    r.setInteger(ic, extra.getCount());
+                }
+            }
+        }
+        return root;
+    }
+
+    /**
+     * Builds a CSV from the values in the node.
+     *
+     * @param node the
+     * @return the CSV
+     */
+    public CSV buildCSV(final Node node) {
+        CSV csv = new CSV();
+        System.out.println("aggregateList: " + aggregateList.size());
+        for (Aggregate a : aggregateList) {
+            if (a instanceof Name) {
+                Name n = (Name) a;
+                StringColumn sc = new StringColumn(a.getColumn().getName());
+//                a.setAggregateColumn(sc);
+                csv.addColumn(sc);
+            } else {
+                IntegerColumn ic = new IntegerColumn(a.getClass().getSimpleName()+ ":" + a.getColumn().getName());
+                a.setAggregateColumn(ic);
+                csv.addColumn(ic);
+            }
+        }
+        Deque<Object> list = new ArrayDeque<>();
+        build(node, csv, list);
+        return csv;
+    }
+
+    /**
+     * Iterates the whole node tree and adds the values in the node as rows
+     * in the CSV.
+     *
+     * @param n     the node
+     * @param csv   the csv
+     * @param deque the deque of items (stack)
+     */
+    public void build(final Node n, final CSV csv, final Deque<Object> deque) {
+        if (!n.isRoot()) {
+            deque.add(n.getValue());
+        }
+
+
+        if (!n.isRoot()){
+            Column c = csv.getColumn(n.getColumn().getName());
+            if (c == null){
+                c = csv.addColumn(n.getColumn().getName());
+            } else {
+                c = n.getColumn();
+            }
+
+            if (n.isEmpty()){
+                // Display path
+                for (Object o : deque){
+                    System.out.print(o + " -> ");
+                }
+                System.out.println(n.getCount() + "\tColumns " + csv.getColumnCount() + " "  + c);
+                Row r = csv.addRow();
+
+                int x = 0;
+                for (Object o : deque) {
+                    Column sc = csv.getColumn(x);
+                    if (sc instanceof StringColumn) {
+                        r.setString((StringColumn) sc, (String) o);
+                    }
+                    x++;
+                }
+    //            for (Aggregate a : aggregateList) {
+    //                if (a instanceof Count) {
+    //                    Count c = (Count) a;
+    //                    IntegerColumn ic = c.getAggregateColumn();
+    //                    r.setInteger(a.getAggregateColumn(), n.getCount());
+    //                }
+    //            }
+    //        if (n.isEmpty()) {
+    //            for (Aggregate a : aggregateList) {
+    //                if (a instanceof Count) {
+    //                    Count c = (Count) a;
+    //                    IntegerColumn ic = c.getAggregateColumn();
+    //                    r.setInteger(a.getAggregateColumn(), n.getCount());
+    //                }
+    //            }
+    //        }
+            }
+        }
+
+        // Iterate the rest of the hierarchy
+        for (Object key : n.getMap().keySet()) {
+            Node subNode = n.getMap().get(key);
+            build(subNode, csv, deque);
+        }
+
+        if (!n.isRoot()) {
+            deque.removeLast();
+        }
+    }
 
 
 }
--- src/test/java/org/laukvik/csv/report/ReportTest.java
+++ src/test/java/org/laukvik/csv/report/ReportTest.java
@@ -6,6 +6,9 @@ import org.laukvik.csv.columns.IntegerColumn;
 import org.laukvik.csv.columns.StringColumn;
 
 import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  *
@@ -77,6 +80,60 @@ public class ReportTest {
 
     }
 
+    @Test
+    public void buildCSV() throws IOException {
+        StringColumn land = new StringColumn("land");
+        StringColumn by = new StringColumn("by");
+
+        Node root = new Node();
+        root.add("Norge",  land).add("Oslo",by);
+        root.add("Norge",  land).add("Tromsø",by);
+        root.add("Sverige",land).add("Arlanda",by);
+        root.add("Norge",  land).add("Bergen",by);
+        root.add("Danmark",land).add("København",by);
+        root.add("Sverige",land).add("Stockholm",by);
+
+        Report r = new Report();
+        CSV csv = r.buildCSV(root);
+        assertEquals( 2, csv.getColumnCount() );
+        assertEquals( land.getName(), csv.getColumn(0).getName() );
+        assertEquals( by.getName(), csv.getColumn(1).getName() );
+
+        System.out.println("TABELL:");
+        for (int y=0; y < csv.getRowCount(); y++){
+            for (int x=0; x<csv.getColumnCount(); x++){
+                System.out.print( csv.getRow(y).get( csv.getColumn(x)) + "\t");
+            }
+            System.out.println();
+        }
+
+        assertEquals( 6, csv.getRowCount() );
+
+    }
+
+    @Test
+    public void buildCSV_node() throws IOException {
+        CSV csv = new CSV();
+        csv.readFile(getResource("metadata.csv"));
+        IntegerColumn presidency = (IntegerColumn) csv.getColumn("presidency");
+        StringColumn party = (StringColumn) csv.getColumn("Party");
+        StringColumn homeState = (StringColumn) csv.getColumn("Home state");
+
+        Report r = new Report();
+        r.addGroup(party);
+//        r.addGroup(homeState);
+
+        r.addColumn(new Name(party));
+//        r.addColumn(new Name(homeState));
+        r.addColumn(new Count(party));
+//        r.addColumn(new Count(homeState));
+
+//        r.addMatcher( new IntLessThanMatcher(presidency, 11));
+        Node root = r.buildNode(csv);
+
+        CSV csv2 = r.buildCSV(root);
+        csv2.writeFile(new File("/Users/morten/Desktop/aggreg.csv"));
+    }
 
 
 }
